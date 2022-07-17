e2d11b4beeb125bdfe5a3685210b535b3dbdd5ef
Ecojod <95634177+Ecojod@users.noreply.github.com>
2022-07-17 12:54:20 +0530

    First spinner done

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -2,14 +2,16 @@ import React from 'react'
 export default function Spinner() {
     return (
         <>
-            <section className="main">
+            <section className="main w-fit mx-auto ">
                 <div className="spinner flex justify-center h-80 w-80 items-center overflow-hidden relative">
                     <h1 className='text-3xl w-fit'>Loading</h1>
                     <div className="spinner-sector absolute w-full h-full spinner-sector-red"></div>
                     <div className="spinner-sector absolute w-full h-full spinner-sector-blue"></div>
                     <div className="spinner-sector absolute w-full h-full spinner-sector-green"></div>
+                    <div className="spinner-sector absolute w-full h-full spinner-sector-black"></div>
+                    <div className="spinner-sector absolute w-full h-full spinner-sector-maroon"></div>
                 </div>
-                <div className="spinner-2">
+                <div className="spinner-2 flex justify-center h-80 w-80 items-center overflow-hidden relative">
                     <h1 className='text-3xl w-fit'>Loading</h1>
                 </div>
             </section>
